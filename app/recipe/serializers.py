86a6f671ec83a8ebe57054c2796843726b18cfdc
arkan-leki <arkan.leki@gmail.com>
2023-07-08 12:19:36 +0300

    create tag issue

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -25,12 +25,15 @@ class RecipeSerializer(serializers.ModelSerializer):
         read_only_fields = ('id',)
 
     def create(self, validated_data):
-        """Create a new recipe."""
+        """Create a recipe."""
         tags = validated_data.pop('tags', [])
         recipe = Recipe.objects.create(**validated_data)
-        auth_user = self.context.get('request').user
+        auth_user = self.context['request'].user
         for tag in tags:
-            tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
             recipe.tags.add(tag_obj)
 
         return recipe
